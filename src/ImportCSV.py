ae4c48372bb2c28594da67b46dc5bedb1d68693a
Antoine Drouard <antoine.drouard@etu.univ-nantes.fr>
2017-03-23 10:47:42 +0100

    Finalisation de l'import

--- src/ImportCSV.py
+++ src/ImportCSV.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import csv
 import sqlite3
-import sys
 
 con = sqlite3.connect('test.db')
 cur = con.cursor()
@@ -23,17 +22,21 @@ except sqlite3.Error:
 
 
 #Prepare the statment to insert values into the database
-sql = ('INSERT INTO installation (numero, nom, adresse, code_postal, ville) VALUES ' '(?, ?, ?, ?, ?)')
+sql = ('INSERT INTO installation (numero, nom, adresse, code_postal, ville, latitude, longitude) VALUES ' '(?, ?, ?, ?, ?, ?, ?)')
 
-# Insert each row of data from installation.csv
+# Insert each row of data from the csv
 installations_data = csv.reader(open("data/csv/installationsMod.csv", "r"))
 for row in installations_data:
-	cur.execute(sql, (row[1], row[0], row[6]+" "+row[7], row[4], row[2], 0.0, 0.0))
+	cur.execute(sql, (row[1], row[0], row[6]+" "+row[7], row[4], row[2], row[10], row[9]))
 
-sql = ('INSERT INTO activite (numero, nom) VALUES ' '(?, ?)')
+sql = ('INSERT OR IGNORE INTO activite (numero, nom) VALUES ' '(?, ?)')
+sql2 = ('SELECT * FROM activite WHERE numero IN (?)')
 activite_data = csv.reader(open("data/csv/activiteMod.csv", "r"))
 for row in activite_data:
-	cur.execute(sql, (row[2], row[0], row[6]+" "+row[7], row[4], row[2]))
+	if((str(row[4]) != "") & (str(row[5]) != "")):
+		cur.execute(sql2, (row[4],))
+		if cur.rowcount:
+			cur.execute(sql, (row[4], row[5]))
 
 sql = ('INSERT INTO equipement (numero, nom, numero_installation) VALUES ' '(?, ?, ?)')
 equipement_data = csv.reader(open("data/csv/equipementsMod.csv", "r"))
@@ -41,14 +44,12 @@ for row in equipement_data:
 	cur.execute(sql, (row[4], row[5], row[2]))
 
 sql = ('INSERT INTO equipement_activite (numero_equipement, numero_activite) VALUES ' '(?, ?)')
-for row in activite_data:
-	for raw in equipement_data:
-		cur.execute(sql, (raw[2], row[0]))
+activite_data2 = csv.reader(open("data/csv/activiteMod.csv", "r"))
+for row in activite_data2:
+	cur.execute(sql, (row[2], row[4]))
 
 
-
-
-cur.execute("SELECT * FROM installation")
+cur.execute("SELECT * FROM equipement_activite")
 for row in cur:
 	print(row)
 
