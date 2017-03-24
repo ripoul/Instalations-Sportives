5e8c47bf5d16c27f6f391d9df0a5dd1e3d5caa0f
Jules LE BRIS <jules.lebris@neuf.fr>
2017-03-24 10:46:38 +0100

    ajout sport

--- src/IHM/Interface.tpl
+++ src/IHM/Interface.tpl
@@ -19,6 +19,12 @@ Type de sport :
   <option value="Course d'orientation">Course d'orientation</option>
   <option value="Tennis de table">Tennis de table</option>
   <option value="Bicross (BMX)">Bicross (BMX)</option>
+  <option value="Tennis">Tennis</option>
+  <option value="Basket-Ball">Basket-Ball</option>
+  <option value="Football / Football en salle (Futsal)">Football</option>
+  <option value="Handball / Mini hand / Handball de plage">HandBall</option>
+  <option value="Musculation">Musculation</option>
+  <option value="Gymnastique Aérobic">Gymnastique Aérobic</option>  
 </select>
 
 
--- src/REST/serveur.py
+++ src/REST/serveur.py
@@ -11,12 +11,13 @@ def installation():
 	c.execute("SELECT i.nom, i.adresse, i.code_postal, i.ville from installation i, equipement e, equipement_activite ea, activite a where i.numero=e.numero_installation and e.numero=ea.numero_equipement and ea.numero_activite=a.numero and a.nom=\""+sport+"\" and i.ville=\""+ville+"\";")
 	data = c.fetchall()
 	c.close()
-	output = template('resultat', rows=data)
+	output = template('src/IHM/resultat.tpl', rows=data)
 	return output
 
 @route('/')
 def installation():
 	#return template('Forum ID: {{id}} (page {{page}})', id=forum_id, page=page)
-	return "welcome"
+	output = template('src/IHM/Interface.tpl')
+	return output
 
 run(host='localhost', port=9999, debug=True)
